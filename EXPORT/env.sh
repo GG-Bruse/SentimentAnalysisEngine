4fa471d6ac9f6a328bdc5e65c0151f43b8a9c709
baojiayi <baojiayi@ishumei.com>
2024-12-25 19:37:43 +0800

    export

--- EXPORT/env.sh
+++ EXPORT/env.sh
@@ -2,4 +2,5 @@ export ckpt_file=./model/1733990412.ckpt
 export model_conf_file=./model/config.json
 export vocab_file=./model/vocab.txt
 export output_numbers=2
+export inference_file=./model/python_inference_result.csv
 export inference_count=10000
--- EXPORT/run.sh
+++ EXPORT/run.sh
@@ -9,7 +9,16 @@ echo $vocab_file
 
 # build engine
 python ./builder/builder.py --config_file ${model_conf_file} --output_path './output/model.engine' --ckpt_file ${ckpt_file}
+
 # engine inference
+if [ -f "./inference/test.txt" ];then
+    echo "test.txt 存在"
+else 
+    echo "test.txt 不存在"
+    cp ../BertModel/dataset/test.txt ./inference/
+fi
 python ./inference/inference.py --input_path './inference/test.txt' --engine_path './output/model.engine' --vocab_file ${vocab_file} --inference_count ${inference_count}
+
+
 # python inference and engine inference diff
 
