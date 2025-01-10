138254b23c7af903a5d5675b3ffcedfc95ecbdac
GG-Bruse <BJY811@outlook.com>
2025-01-10 16:57:07 +0800

    flag

--- EXPORT/env.sh
+++ EXPORT/env.sh
@@ -1,7 +1,7 @@
-export ckpt_file=./model/1736418888.ckpt
+export ckpt_file=./model/1736490800.ckpt
 export model_conf_file=./model/config.json
 export vocab_file=./model/vocab.txt
-export output_numbers=7
-export output_classes=sadness,happiness,disgust,anger,like,surprise,fear
+export output_numbers=2
+export output_classes=Negative,Positive
 export inference_file=./model/python_inference_result.csv
-export inference_count=3178
+export inference_count=10000
